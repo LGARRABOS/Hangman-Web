d69e52e3ffbb386fb7b247eace2b1643aa2a95a4
AFERREIRA33 <alex.ferreira@ynov.com>
2022-01-07 09:35:55 +0100

    main change and fixe hangman classic

--- hangman-classic/function/random.go
+++ hangman-classic/function/random.go
@@ -11,7 +11,7 @@ func RandomWord() string {
 	rand.Seed(time.Now().UnixNano())
 	var wordtab []string
 	var word string
-	f, err := os.Open("words.txt")
+	f, err := os.Open("hangman-classic/function/words.txt")
     if err != nil {
         log.Fatal(err)
     }
--- main.go
+++ main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-		"html/template"
+		/*"html/template"
         "log"
         "net/http"
-		"fmt"
+		"fmt"*/
+		hangman "hangman/hangman-classic/hangman"
 )
 func main() {
-        // Set routing rules
+        /*// Set routing rules
         http.HandleFunc("/", Tmp)
 
         //Use the default DefaultServeMux.
         err := http.ListenAndServe(":8080", nil)
         if err != nil {
                 log.Fatal(err)
-        }
-}
+        }*/
+		hangman.Hangman();
 
+}
+/*
 func Tmp(w http.ResponseWriter, r *http.Request) { 
 	tmpl, err := template.ParseFiles("./index.html")
 	if err != nil {
 		fmt.Println(err)
 	}
 	err = tmpl.Execute(w , nil)
-}
+}*/
