b17db1a9df9503b65a6a35a11d3b4a6269b7d0a2
AFERREIRA33 <alex.ferreira@ynov.com>
2022-01-13 09:37:28 +0100

    envoie donnée sur web / chemin pour les fichiers

---
+++ server/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+		"html/template"
+        "net/http"
+		"fmt"
+		hangman "hangman/hangman"
+)
+type Data struct {
+	LetterChoose string
+	Attempts int
+	TabUnderscore string
+	Won string
+
+}
+
+func main() {
+	http.HandleFunc("/", Handler)
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	
+
+	http.HandleFunc("/hangman", Handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.Must(template.ParseFiles("../index.html"))
+	letter := ""
+	var hidden_word string
+	var attempts int
+	var win bool
+	won := ""
+	switch r.Method {
+	case "GET" :
+		fmt.Println("GET METHOD")
+	case "POST" :
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		fmt.Printf("POST METHOD = %v\n", r.PostForm)
+		m := r.PostForm
+		l := m["letter"]
+		letter = l[0]		
+	}
+	if len(letter) == 0  {
+		hidden_word, attempts, win = hangman.Hangman(letter, true)
+		letter = " "
+	} else {
+		hidden_word, attempts, win = hangman.Hangman(letter, false)
+	}
+	if win {
+		won = "Congrats !"
+	}
+	data := Data {
+		LetterChoose: letter,
+		Attempts: attempts,
+		TabUnderscore: hidden_word,
+		Won: won,
+	}
+	fmt.Println(hidden_word, attempts, win)
+	tmpl.Execute(w, data)
+}
