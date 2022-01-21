c15685683ce17d206857afda5eec98b6fb02551f
JSELVA <joseph.selva@ynov.com>
2022-01-21 11:09:10 +0100

    html and css image fix

--- server/main.go
+++ server/main.go
@@ -26,7 +26,7 @@ func main() {
 	fs := http.FileServer(http.Dir("../static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe("localhost:8080", nil)
 
 }
 
